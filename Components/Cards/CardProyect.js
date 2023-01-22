553453a21456a4f67ed52c8b5771d5e8e39b7f66
josephsb <jguku2002@gmail.com>
2023-01-22 12:51:45 -0500

    changes

--- Components/Cards/CardProyect.js
+++ Components/Cards/CardProyect.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import ItemTool from "../Items/ItemTool";
 import Btn1_SPA from "@Components/Buttons/Btn1_SPA";
+import Link from "next/link";
 
 const CardProyect = ({
   portadas,
@@ -11,44 +12,41 @@ const CardProyect = ({
   id,
 }) => {
   return (
-    <div className="CardProyect">
-      <img
-        crossOrigin="anonymous"
-        src={portadas[0]}
-        alt={name}
-        width={280}
-        height={140}
-      />
-      <div className="CardProyect_Content">
-        <h3 className="CardProyect_Tittle">{name}</h3>
-        <ul className="CardProyect_MenuTags">
-          {tags.map((tag, index) => (
-            <li key={index} className="CardProyect_tag">
-              {tag}
-            </li>
-          ))}
-        </ul>
-        <p className="CardProyect_description">{description}</p>
-      </div>
-      <div className="CardProyect_Links">
-        <Btn1_SPA
-          icon="svg/next-double.svg"
-          message="Ver mas"
-          route={`/Proyects/${id}`}
-        />
-      </div>
-      <div className="CardProyect_footer">
-        {technologies.map((item, index) =>
-          item.icon.length > 0 ? (
-            <ItemTool key={index} {...item} />
-          ) : (
-            <p key={index} className="CardSkill_SpanIcon">
-              {item.name}
-            </p>
-          )
-        )}
-      </div>
-    </div>
+    <Link href={`/Proyects/${id}`}>
+      <a>
+        <div className="CardProyect">
+          <img
+            crossOrigin="anonymous"
+            src={portadas[0]}
+            alt={name}
+            width={280}
+            height={140}
+          />
+          <div className="CardProyect_Content">
+            <h3 className="CardProyect_Tittle">{name}</h3>
+            <ul className="CardProyect_MenuTags">
+              {tags.map((tag, index) => (
+                <li key={index} className="CardProyect_tag">
+                  {tag}
+                </li>
+              ))}
+            </ul>
+            <p className="CardProyect_description">{description}</p>
+          </div>
+          <div className="CardProyect_footer">
+            {technologies.map((item, index) =>
+              item.icon.length > 0 ? (
+                <ItemTool key={index} {...item} />
+              ) : (
+                <p key={index} className="CardSkill_SpanIcon">
+                  {item.name}
+                </p>
+              )
+            )}
+          </div>
+        </div>
+      </a>
+    </Link>
   );
 };
 
